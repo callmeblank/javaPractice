a357adbd4b2bc6ae056c3395a77a0823ada5062b
Phuoc <chemlink2006@gmail.com>
2025-06-30 11:23:44 +0700

    Wolf extends Cannie

--- Chap8/Cannie.java
+++ Chap8/Cannie.java
@@ -1,5 +1,7 @@
 package Chap8;
 
-public class Cannie {
-    
+public abstract class Cannie extends Animal{
+    //Cannie is also an abstract that is subclass of Animal.
+    //However Cannie don't have to override abstract method, but oc you can override it
+    public void age(){System.out.println("five years old");} 
 }
--- Chap8/Wolf.java
+++ Chap8/Wolf.java
@@ -1,5 +1,10 @@
 package Chap8;
 
-public class Wolf {
+public class Wolf extends Cannie{
+    //because we override age in Cannie class. so we don't need to override it again.that makes sense
+    public static void main(String[] args){
+        Wolf wolf = new Wolf();
+        wolf.age();
+    }
     
 }
