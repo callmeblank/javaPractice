9a95e247549ef78bf6f7a537c21611a1162d2c07
Phuoc <chemlink2006@gmail.com>
2025-06-30 11:04:08 +0700

    abstract a Class

--- Chap8/Animal.java
+++ Chap8/Animal.java
@@ -1,13 +1,12 @@
 package Chap8;
 
-public class Animal {
+abstract public class Animal {
     public void roam(){System.out.println("Walking ...");}
     public void eat(){System.out.println("Mukbang...");}
     public void sleep(){System.out.println("Sleeping...");}
     public static void main(String[] args){
-        Animal anm = new Animal();
-        anm.roam();
-        anm.eat();
-        anm.sleep();
+        //it's not reasonable to instantiate an animal (How is it look like. what is it?)
+        //so make it abstract prevent it from being instantiated
+        System.out.println("Wellcome to the zoo");
     }
 }
