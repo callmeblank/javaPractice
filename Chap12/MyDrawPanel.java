53162b00041f9f12e8c0ff817ec0940c0786d04c
Phuoc <chemlink2006@gmail.com>
2025-07-10 01:00:58 +0700

    add cButton feature

--- Chap12/MyDrawPanel.java
+++ Chap12/MyDrawPanel.java
@@ -2,20 +2,33 @@ package Chap12;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-public class MyDrawPanel extends JPanel  {
-    
+public class MyDrawPanel extends JPanel implements ActionListener {
+    JFrame frame;
     public static void main(String[] args){
-        JFrame frame = new JFrame();
-
-        //add MyDrawPanel
         MyDrawPanel mdPanel = new MyDrawPanel();
-        frame.getContentPane().add(mdPanel);
+        mdPanel.go();
+    }
+    public void go(){
+        frame = new JFrame();
+
+        //add MyDrawPanel and buttons
+        MyDrawPanel mdPanel = new MyDrawPanel();
+        JButton cButton = new JButton("Change color");
+        frame.getContentPane().add(BorderLayout.CENTER, mdPanel);
+        frame.getContentPane().add(BorderLayout.SOUTH, cButton);
+        //add listener here
+        cButton.addActionListener(this);
+        
 
         //set things
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(300, 300);
         frame.setVisible(true);
+    }
+    public void actionPerformed(ActionEvent e){
+        frame.repaint();
     }
     //this is abstract method from JPanel you have to implement. 
     //And you DON'T call it, it is called automatically, g is an object of Graphics2D (subclass of Graphics).
