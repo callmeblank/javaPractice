9d36195033fb4a64e131307f83b29b7f9517d142
Phuoc <chemlink2006@gmail.com>
2025-07-10 00:32:53 +0700

    gradient

--- Chap12/MyDrawPanel.java
+++ Chap12/MyDrawPanel.java
@@ -20,17 +20,28 @@ public class MyDrawPanel extends JPanel  {
     //this is abstract method from JPanel you have to implement. 
     //And you DON'T call it, it is called automatically, g is an object of Graphics2D (subclass of Graphics).
     public void paintComponent(Graphics g){
+        Graphics2D g2d = (Graphics2D) g;
         //paint background of this
-        g.fillRect(0, 0, this.getWidth(), this.getHeight());
+        g2d.fillRect(0, 0, this.getWidth(), this.getHeight());
 
         //make random Color
-        int red = (int) (Math.random() * 256);
-        int green = (int) (Math.random() * 256);
-        int blue = (int) (Math.random() * 256);
-        Color rColor = new Color(red, green, blue);
+        //start color
+        int sred = (int) (Math.random() * 256);
+        int sgreen = (int) (Math.random() * 256);
+        int sblue = (int) (Math.random() * 256);
+        Color sColor = new Color(sred, sgreen, sblue);
+
+        //end color
+        int ered = (int) (Math.random() * 256);
+        int egreen = (int) (Math.random() * 256);
+        int eblue = (int) (Math.random() * 256);
+        Color eColor = new Color(ered, egreen, eblue);
+
+        //create gradient color
+        GradientPaint gradientColor = new GradientPaint(70, 70, sColor, 150, 150, eColor);
 
         //set random color
-        g.setColor(rColor);
-        g.fillOval(20, 50, 100, 100);
+        g2d.setPaint(gradientColor);
+        g2d.fillOval(20, 50, 100, 100);
     }
 }
