7a76c07e557c26506e5a9ff4b054b6c10eb7550c
Phuoc <chemlink2006@gmail.com>
2025-07-10 00:01:35 +0700

    modify alistener file

--- Chap12/AListener.java
+++ Chap12/AListener.java
@@ -2,6 +2,7 @@ package Chap12;
 import java.awt.event.*;
 
 public class AListener implements ActionListener{
+    //i linked this AListener to button in SimpleGui1B to show that i can connect whatever i want
     public void actionPerformed(ActionEvent ae){
         System.out.println("Clicked");
     }
