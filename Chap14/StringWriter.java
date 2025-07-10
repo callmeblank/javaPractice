40a3b372527beafe950b1e38fd1e3be5d6f94241
Phuoc <chemlink2006@gmail.com>
2025-07-10 19:27:17 +0700

    File Object

--- Chap14/StringWriter.java
+++ Chap14/StringWriter.java
@@ -1,12 +1,16 @@
 package Chap14;
 
 import java.io.FileWriter;
+import java.io.File;
 
 public class StringWriter {
     public static void main(String[] args){
         try{
-            FileWriter fw = new FileWriter("myName.txt");
-            fw.write("phuocNguyen");
+            //i can use File to represent the string and still have ability to 
+            //pass File object as parameter of Constructor of FileWriter
+            File pathFile = new File("myName.txt");
+            FileWriter fw = new FileWriter(pathFile);
+            fw.write("Hummle");
             fw.close();
         }catch(Exception ex){ex.printStackTrace();}
     }
