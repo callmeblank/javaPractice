6c722cc85db334c38d5348bbde02db852d316a51
Phuoc <chemlink2006@gmail.com>
2025-07-10 19:35:06 +0700

    some methods of File Object

--- Chap14/StringWriter.java
+++ Chap14/StringWriter.java
@@ -11,6 +11,19 @@ public class StringWriter {
             File pathFile = new File("myName.txt");
             FileWriter fw = new FileWriter(pathFile);
             fw.write("Hummle");
+            //or we can create a folder
+            File folderName = new File("Solder");
+            // folderName.mkdir();
+            //get absolute path
+            System.out.println("Absolute path: " + folderName.getAbsolutePath());
+            System.out.println("");
+            //list contents of folder
+            String[] Solders = folderName.list();
+            for (int i = 0; i < Solders.length; ++i){
+                int index = i + 1;
+                System.out.println("Content number " + index + ": " + Solders[i]);
+            }
+            //and many more things
             fw.close();
         }catch(Exception ex){ex.printStackTrace();}
     }
